b2f5144e992068ca42045ec3ef2f2d38988aafd1
Christina Deemer <cahdeemer@gmail.com>
2016-06-09 00:16:38 +0000

    week 2 exercises

---
+++ variables.php
@@ -0,0 +1,48 @@
+<?php 
+$firstName = 'Christina';
+$lastName = 'Deemer';
+
+echo '<h1>' . $firstName . ' ' . $lastName . '</h1>'; 
+
+$n = 5;
+$j = 6;
+
+echo '<p>' . $n + $j . '</p>';
+
+if ($n < $j) {
+    echo $n . ' is a smaller number than ' . $j; 
+} elseif ($n > $j) {
+    echo $n . ' is a larger number than '  . $j;
+} elseif ($n == $j) {
+    echo $n . ' is equal to ' . j;
+} else {
+    echo 'maybe ' . $n . ' and ' . $j . 'are not numbers';
+}
+
+?>
+
+<?php $n = 7; ?>
+
+<p><?php echo $n + $j; ?></p>
+
+<p>
+    <?php
+    
+    if ($n < $j) {
+    echo $n . ' is a smaller number than ' . $j; 
+    } elseif ($n > $j) {
+    echo $n . ' is a larger number than '  . $j;
+    } elseif ($n == $j) {
+    echo $n . ' is equal to ' . j;
+    } else {
+    echo 'maybe ' . $n . ' and ' . $j . 'are not numbers';
+    };
+
+    ?>
+    
+    
+</p>
+
+
+
+
